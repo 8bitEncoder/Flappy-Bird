956b2009e90d6235eb118c0353cd00ad26165ae3
8bitEncoder <fahmipro4646@gmail.com>
2026-06-14 08:10:31 +0800

    Added jump mechanics for bird

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,14 +2,14 @@ package src;
 
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GamePanel extends JPanel implements ActionListener {
+public class GamePanel extends JPanel implements KeyListener {
     //Data members
     private Image bg;
     private Bird bird;
@@ -21,8 +21,19 @@ public class GamePanel extends JPanel implements ActionListener {
         bg = new ImageIcon(getClass().getResource("/assets/img/flappybirdbg.png")).getImage();
         bird = new Bird(new ImageIcon(getClass().getResource("/assets/img/flappybird.png")).getImage());
         pipe = new Pipe(new ImageIcon(getClass().getResource("/assets/img/bottomPipe.png")).getImage());
-        timer = new Timer(16, this); // ~60 FPS
+        setFocusable(true);
+        addKeyListener(this);
+        timer = new Timer(16, e -> {
+            bird.update();
+            pipe.move();
+            if (pipe.isOffScreen()) {
+                System.out.println("pipe is off screen");
+                pipe.reset();
+            }
+            repaint();
+        }); // ~60 FPS
         timer.start();
+        
     }
 
     protected void paintComponent(Graphics g) {
@@ -33,13 +44,14 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        bird.update();
-        pipe.move();
-        if (pipe.isOffScreen()) {
-            System.out.println("pipe is off screen");
-            pipe.reset();
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            bird.jump();
         }
-        repaint();
     }
+    
+    @Override
+    public void keyTyped(KeyEvent e) {}
+    @Override
+    public void keyReleased(KeyEvent e) {}
 }                       
